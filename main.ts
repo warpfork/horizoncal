1872afc52b3c1b6f68315e32cecfd7ffd5e3e40a
Eric Myhre <hash@exultant.us>
2024-02-11 22:59:37 +0100

    i feel both shame and none about this.

--- main.ts
+++ main.ts
@@ -1,32 +1,39 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	ItemView,
+	Modal,
+	Plugin, PluginSettingTab, Setting,
+	WorkspaceLeaf
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface HorizonCalSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: HorizonCalSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const VIEW_TYPE = "horizoncal-view";
+
+export default class HorizonCalPlugin extends Plugin {
+	settings: HorizonCalSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Open Horizon Calendar', (evt: MouseEvent) => {
+			//new Notice('This is a notice!');
+			this.activateView();
 		});
-		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Views!  This is what I'm here for.
+		this.registerView(
+			VIEW_TYPE,
+			(leaf) => new HorizonCalView(leaf)
+		);
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -36,37 +43,9 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new HorizonCalSettingsTabMain(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -89,7 +68,88 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	// All the function names above are magical.
+
+	async activateView() {
+		const { workspace } = this.app;
+	  
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE);
+	  
+		if (leaves.length > 0) {
+		  // A leaf with our view already exists, use that
+		  leaf = leaves[0];
+		} else {
+		  // Our view could not be found in the workspace, create a new leaf
+		  // in the right sidebar for it
+		  // TODO this is... not what I want.  What's the "main" leaf?
+		  leaf = workspace.getRightLeaf(false);
+		  await leaf.setViewState({ type: VIEW_TYPE, active: true });
+		}
+	  
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf);
+	    }
 }
+
+export class HorizonCalView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+	  super(leaf);
+	}
+    
+	getViewType() {
+	  return VIEW_TYPE;
+	}
+    
+	getDisplayText() {
+	  return "This is the hovertext on the icon.";
+	}
+    
+	async onOpen() {
+	  const container = this.containerEl.children[1];
+	  container.empty();
+	  container.createEl("h4", { text: "Example view" });
+	  const {FullCalendar} = customJS;
+	  container.createEl("span", { text: "lol"+FullCalendar });
+	  const rootNode = container.createEl("div");
+
+	  var fc = FullCalendar.bonk()
+	  var cal = new fc.Calendar(rootNode, {
+		  //plugins: [fc.timegrid], // not when vendorstyle apparently.
+		  initialView: 'timeGridFourDay',
+		  views: {
+			  timeGridFourDay: {
+				  type: 'timeGrid',
+				  duration: { days: 12 }
+			  },
+		  },
+		  eventSources: [
+			  {
+				  events: [
+					  {
+						  title: 'Event1',
+						  start: '2024-02-11'
+					  },
+					  {
+						  title: 'Event2',
+						  start: '2024-02-11T12:30:00',
+						  end: '2024-02-11T13:30:00'
+					  }
+				  ],
+				  color: 'yellow',   // an option!
+				  textColor: 'black' // an option!
+			  }
+		  ],
+	  })
+	  cal.render()
+	}
+    
+	async onClose() {
+	  // Nothing to clean up.
+	}
+    }
+    
 
 class SampleModal extends Modal {
 	constructor(app: App) {
@@ -98,7 +158,7 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Woah!  Really.');
 	}
 
 	onClose() {
@@ -107,10 +167,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class HorizonCalSettingsTabMain extends PluginSettingTab {
+	plugin: HorizonCalPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HorizonCalPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
