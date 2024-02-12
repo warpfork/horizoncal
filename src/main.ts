3a62275452352c0560d52a56de743430587193d8
Eric Myhre <hash@exultant.us>
2024-02-12 22:32:56 +0100

    discard some demo garbage

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import {
-	App,
-	Modal,
 	Plugin,
 	WorkspaceLeaf
 } from 'obsidian';
@@ -27,26 +25,8 @@ export default class HorizonCalPlugin extends Plugin {
 			(leaf) => new HorizonCalView(leaf)
 		);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new HorizonCalSettingsTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -85,21 +65,5 @@ export default class HorizonCalPlugin extends Plugin {
 
 		// "Reveal" the leaf in case it is in a collapsed sidebar
 		workspace.revealLeaf(leaf);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!  Really.');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
