2a2ce27cb2e02a1438906c2dff477536fbcdb4f4
Eric Myhre <hash@exultant.us>
2024-02-17 12:28:36 +0100

    Do that dance with one less spare object.

--- src/view.ts
+++ src/view.ts
@@ -247,60 +247,56 @@ export class HorizonCalView extends ItemView {
 				let newEndDt = toLuxonDateTime(info.event.end as Date, this.calUI).setZone(evtFm.endTZ || evtFm.evtTZ)
 
 				// Now start modifying the frontmatter...
-				// But we're going to do this by making a new object.
-				// We're going to append all properties we manage, in order.
-				// Then we're going to merge back any remaining properties at the end.
+				// And kick this off in an odd way: because we want to control order,
+				// we're going to nuke every property and put them back in again.
+				// We'll keep an object on the side with a copy of the originals,
+				// then merge back any remaining properties that aren't ours at the end.
 				// This dance creates stable ordering (and shifts user content to the bottom).
-				let newFm: any = {}
-
-				// Copy the most essential traits, like title and type!
-				newFm.title = evtFm.title
-				newFm.evtType = evtFm.evtType
-				// The start date is always written!
-				newFm.evtDate = newStartDt.toFormat("yyyy-MM-dd")
-				// The start time should only be written if it was there before, or if it's now nonzero.
-				// (All day events have zero here, and should have stored no time.)
-				if (evtFm.evtTime || newStartDt.hour != 0 || newStartDt.minute != 0) {
-					newFm.evtTime = newStartDt.toFormat("HH:mm")
-				}
-				// Copy the TZ.
-				// Or if it didn't exist: it does now!
-				newFm.evtTZ = (evtFm.evtTZ || "CET"); // FIXME
-				// Write the end date only if it's different than the start date.
-				if (newStartDt.year != newEndDt.year || newStartDt.month != newEndDt.month || newStartDt.day != newEndDt.day) {
-					newFm.endDate = newEndDt.toFormat("yyyy-MM-dd")
-				}
-				// As with start time: the end time should only be written if it was there before, or if it's now nonzero.
-				// (All day events have zero here, and should have stored no time.)
-				if (evtFm.endTime || newEndDt.hour != 0 || newEndDt.minute != 0) {
-					newFm.endTime = newEndDt.toFormat("HH:mm")
-				}
-				// Copy the TZ, if it existed.
-				if (evtFm.endTZ) {
-					newFm.endTZ = evtFm.endTZ
-				}
-				// Copy our other known but optional properties, if they existed.
-				// (Be careful with equality here, as some of these are bools.)
-				if (evtFm.allDay !== undefined) {
-					newFm.allDay = evtFm.allDay
-				}
-				if (evtFm.completed !== undefined) {
-					newFm.completed = evtFm.completed
-				}
-
-				// Now replace the heck out of properties, such that we win at order.
-				// This looks kind of comical, but is the only way I can figure
-				// that updates the original object such that the callback has effects.
-				// I sure would love if there was just an "insertAfter" feature,
-				// given that the internal behavior of this stuff IS specified these days.
-				let copy = Object.assign({}, evtFm);
+				let refFm = Object.assign({}, evtFm);
 				for (var prop in evtFm) {
 					delete evtFm[prop]
 				}
-				Object.assign(evtFm, newFm);
-				for (var prop in copy) {
+
+				// Copy the most essential traits, like title and type!
+				evtFm.title = refFm.title
+				evtFm.evtType = refFm.evtType
+				// The start date is always written!
+				evtFm.evtDate = newStartDt.toFormat("yyyy-MM-dd")
+				// The start time should only be written if it was there before, or if it's now nonzero.
+				// (All day events have zero here, and should have stored no time.)
+				if (refFm.evtTime || newStartDt.hour != 0 || newStartDt.minute != 0) {
+					evtFm.evtTime = newStartDt.toFormat("HH:mm")
+				}
+				// Copy the TZ.
+				// Or if it didn't exist: it does now!
+				evtFm.evtTZ = (refFm.evtTZ || "CET"); // FIXME
+				// Write the end date only if it's different than the start date.
+				if (newStartDt.year != newEndDt.year || newStartDt.month != newEndDt.month || newStartDt.day != newEndDt.day) {
+					evtFm.endDate = newEndDt.toFormat("yyyy-MM-dd")
+				}
+				// As with start time: the end time should only be written if it was there before, or if it's now nonzero.
+				// (All day events have zero here, and should have stored no time.)
+				if (refFm.endTime || newEndDt.hour != 0 || newEndDt.minute != 0) {
+					evtFm.endTime = newEndDt.toFormat("HH:mm")
+				}
+				// Copy the TZ, if it existed.
+				if (refFm.endTZ) {
+					evtFm.endTZ = refFm.endTZ
+				}
+				// Copy our other known but optional properties, if they existed.
+				// (Be careful with equality here, as some of these are bools.)
+				if (refFm.allDay !== undefined) {
+					evtFm.allDay = refFm.allDay
+				}
+				if (refFm.completed !== undefined) {
+					evtFm.completed = refFm.completed
+				}
+
+				// Now copy over any remaining properties in the original.
+				// This is part of the dance to control property orders.
+				for (var prop in refFm) {
 					if (!(prop in evtFm)) {
-						evtFm[prop] = copy[prop]
+						evtFm[prop] = refFm[prop]
 					}
 				}
 
@@ -309,7 +305,7 @@ export class HorizonCalView extends ItemView {
 				//  it persists our mutations to the argument.
 
 				// But we also keep this value because we need it in a moment to consider the file's path, as well.
-				evtFm2 = evtFm;
+				evtFm2 = refFm;
 			});
 
 			// Step three: decide if the filename is still applicable or needs to change -- and possibly change it!
