d7f3a8820b3d7c728ea55a02d303e5215eed744a
Eric Myhre <hash@exultant.us>
2025-11-21 15:23:17 +0100

    enable week numbers in all views.
    (I probably would've done this a long time ago if FC's docs and types
    were easier to read!  I'm just finding stuff by probing the
    autocomplete at this point, tbqh.  The TS type math going on inside is
    too wild by half for me to mentally parse.)

--- src/ui/calendarview.ts
+++ src/ui/calendarview.ts
@@ -216,14 +216,25 @@ export class HorizonCalView extends ItemView {
 				left: "",
 			},
 			views: {
+				month: {
+					weekNumbers: true,
+				},
+				week: {
+					weekNumbers: true,
+				},
+				list: {
+					weekNumbers: true, // no apparent effect.
+				},
 				timeGridFourDay: {
 					type: "timeGrid",
+					weekNumbers: true,
 					duration: { days: 4 },
 					dateIncrement: { days: 1 },
 					slotEventOverlap: false,
 				},
 				timeGrid14Day: {
 					type: "timeGrid",
+					weekNumbers: true,
 					duration: { days: 14 },
 					dateIncrement: { days: 1 },
 					slotEventOverlap: false,
